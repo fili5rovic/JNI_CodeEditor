6146105f54477801dfe64f8f69c874594c883c8d
fili5rovic <fili5rovic@gmail.com>
2024-09-29 14:24:45 +0200

    Added VM option for running native code with -Djava.library.path

--- src/main/java/fili5rovic/jni_codeeditor/jni_codeeditor/util/CompileAndRunMultiple.java
+++ src/main/java/fili5rovic/jni_codeeditor/jni_codeeditor/util/CompileAndRunMultiple.java
@@ -10,15 +10,16 @@ import java.util.Collections;
 
 public class CompileAndRunMultiple {
 
-    public static String outputDirectoryPath = ".jnice/isolated-classes";
+    private static String outputDirectoryPath = ".jnice/isolated-classes";
+    private static String libraryPath = "D:\\PROJECTS\\JavaCustomProjects\\JNI_Example_Project\\native";
 
     public static void compileAndRun(String[] sourceFiles) throws IOException {
         cleanOutputDirectory();
         // Obtain system Java compiler (JDK needs to be installed)
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        if (compiler == null) {
+        if (compiler == null)
             throw new IllegalStateException("Cannot find the system Java compiler. Ensure that you are running a JDK, not a JRE.");
-        }
+
         // Set up a file manager for the compiler
         StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
 
@@ -37,21 +38,23 @@ public class CompileAndRunMultiple {
 
         try {
             runCompiledCode(outputDirectory);
-        } catch (InvocationTargetException | IllegalAccessException | ClassNotFoundException | MalformedURLException |
-                 NoSuchMethodException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void runCompiledCode(File outputDirectory) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, ClassNotFoundException, MalformedURLException {
-        // Only use the compiled output directory in the class loader
-        URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{outputDirectory.toURI().toURL()});
 
-        // Load the specific class by its full name
-        Class<?> cls = Class.forName("fili5rovic.jni_codeeditor.jni_codeeditor.Test1", true, classLoader);
-        Method main = cls.getMethod("main", String[].class);
-        main.invoke(null, (Object) new String[]{});
+
+    private static void runCompiledCode(File outputDirectory) throws Exception {
+        // Start a new Java process with the specified library path
+        ProcessBuilder processBuilder = new ProcessBuilder("java", "-Djava.library.path=" + libraryPath, "-cp", outputDirectory.getPath(), "fili5rovic.jni_codeeditor.Main");
+        processBuilder.inheritIO(); // Redirect output to the console
+        Process process = processBuilder.start();
+        int exitCode = process.waitFor(); // Wait for the process to finish
+        System.out.println("Process exited with code: " + exitCode);
     }
+
+
 
     private static void cleanOutputDirectory() {
         File outputDirectory = new File(".jnice/isolated-classes");
@@ -64,9 +67,18 @@ public class CompileAndRunMultiple {
     public static void main(String[] args) throws Exception {
         // Source files to compile (no need for other project dependencies)
         String[] sourceFilesStrings = new String[]{
-                "src/main/java/fili5rovic/jni_codeeditor/jni_codeeditor/Test1.java",
-                "src/main/java/fili5rovic/jni_codeeditor/jni_codeeditor/Test2.java"
+                "D:\\PROJECTS\\JavaCustomProjects\\JNI_Example_Project\\src\\main\\java\\fili5rovic\\jni_codeeditor\\Main.java",
         };
+
+
         compileAndRun(sourceFilesStrings);
+    }
+
+    public static void setLibraryPath(String libraryPath) {
+        CompileAndRunMultiple.libraryPath = libraryPath;
+    }
+
+    public static void setOutputDirectoryPath(String outputDirectoryPath) {
+        CompileAndRunMultiple.outputDirectoryPath = outputDirectoryPath;
     }
 }
